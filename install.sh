acd94990b5fd8f695fec4d212e0021e2ad47d408
seck19 <kelvinkhanh192@gmail.com>
2025-05-11 17:01:48 +0700

    install.sh

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e  # Thoát nếu có lỗi
+
+echo "[*] Cài đặt npm dependencies..."
+npm install
+
+if [ ! -d "LinkFinder" ]; then
+    echo "[*] Cloning LinkFinder..."
+    git clone https://github.com/GerbenJavado/LinkFinder.git 
+else
+    echo "[*] Bỏ qua: LinkFinder đã tồn tại."
+fi
+
+cd LinkFinder
+echo "[*] Cài đặt LinkFinder..."
+sudo python setup.py install
+cd ..
+
+if [ ! -d "trufflehog" ]; then
+    echo "[*] Cloning TruffleHog..."
+    git clone https://github.com/trufflesecurity/trufflehog.git
+else
+    echo "[*] Bỏ qua: trufflehog đã tồn tại."
+fi
+
+echo "[*] Cài đặt TruffleHog..."
+cd trufflehog
+go install
+cd ..
+
+echo "[*] Cài đặt Semgrep..."
+python3 -m pip install semgrep --break-system-packages
+
+echo "[+] Hoàn tất cài đặt!"
